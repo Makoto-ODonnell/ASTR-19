a8da96d25468b2f864badee924f8f48f8316b986
Makoto O'Donnell <mnodonne@ucsc.edu>
2024-09-27 11:30:34 -0700

    Introduction to Python - Code Journal Prompt 1

---
+++ Introduction.py
@@ -0,0 +1,3 @@
+print("Hello, World!")
+
+print("My name is Makoto O'Donnell.\nI go by He/Him pronouns.\n\nI don't really have a favorite movie, but I tend to watch animated films over normal ones. For instance, my favorite show is Arcane. {season 2 is releasing on Netflix next month I highly recommend it!}. Also, my favorite food is burritos.")
