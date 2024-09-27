de6ec18f128199ff6aa3021dbd90afed81b9a4e7
Makoto O'Donnell <mnodonne@ucsc.edu>
2024-09-27 11:54:08 -0700

    Introduction to Python - Journal Prompt #2

---
+++ exercise 2.py
@@ -0,0 +1,18 @@
+a = 50
+b = 25
+c = a - b
+
+d = " is a Float"
+e = " is an Integer"
+
+x = 1.2
+y = 5.5
+z = x + y
+
+print(str(z) + d)
+
+print(str(c) + e)
+
+print(str(y*b) + d)
+
+# I hope this is what you meant by "have the program print out the data type of the resulting answer."
